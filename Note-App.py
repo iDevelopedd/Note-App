483f21a06a963a684bcb0c66fc9f6c4f3b089a37
iDevelopedd <fresdev12@gmail.com>
2022-08-30 18:12:03 -0700

    Base status

---
+++ Note-App.py
@@ -0,0 +1,11 @@
+# Import the Tkinter library
+from tkinter import *
+
+# Set the root of the application
+root = Tk()
+
+Label1 = Label(root, text="Note", width=60, height=30)
+Label1.pack()
+
+root.mainloop()
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Note-App
+# By Araya Karafo & Kevin Alverado
+
+A simple python note-app made with python and tkinter library
+
+

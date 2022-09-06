5984c94485381a4c13ad0e8a1fe5bafc2051b3a4
iDevelopedd <fresdev12@gmail.com>
2022-09-05 17:59:02 -0700

    Modifed the Note-App.py

--- Note-App.py
+++ Note-App.py
@@ -1,6 +1,7 @@
 # Import the Tkinter library
 from tkinter import *
 
+############################################# Start of the application #####################
 # Set the root of the application
 root = Tk()
 
@@ -8,10 +9,23 @@ root.title('Note-App')
 root.geometry("500x500")
 root.config(background="#D9D9D9")
 
+############################################# Setting up the variables and files we need #####################
 # Fonts and color scheme
-inter_bold =  'Inter-Bold'
-inter_regular =  'Inter-Regular'
+inter_bold = 'Inter-Bold'
+inter_regular = 'Inter-Regular'
 figma_white = '#D9D9D9'
+
+# Image importer for the minimize, maximize and exit buttons
+exit_button = PhotoImage(file = r"C:\Users\Prise\Downloads\ExitButton.png")
+minimize_button = PhotoImage(file = r"C:\Users\Prise\Downloads\Minimize.png")
+maximize_button = PhotoImage(file= r"C:\Users\Prise\Downloads\Maximize.png")
+
+# Resizes the images
+exit_button_image = exit_button.subsample(2, 2)
+minimize_button_image = minimize_button.subsample(2, 2)
+maximize_button_image = maximize_button.subsample(2, 2)
+
+###################################### Removing and creating a custom app ################################################
 
 # Remove title bar
 root.overrideredirect(True)
@@ -28,22 +42,78 @@ title_bar.pack(fill=X)
 title_line = Frame(root, bg="black", relief="flat")
 title_line.pack(expand=0, fill=X)
 
-# Function for moving the app
-def move_app(e):
-    root.geometry(f'+{e.x_root}+{e.y_root}')
+# Function to get the current position of the mouse pointer on the screen
+def get_pos(e):
+    xwin = root.winfo_x()
+    ywin = root.winfo_y()
+    startx = e.x_root
+    starty = e.y_root
 
-# Bind the title bar (B1 is the left mouse button <B1-Motion> and motion means that it will print the position of the moving mouse pointer)
-title_bar.bind("<B1-Motion>", move_app)
+    ywin -= starty
+    xwin -= startx
+
+    # Function to move the app window to the mouse pointer
+    def move_app(e):
+        root.geometry('+{0}+{1}'.format(e.x_root + xwin, e.y_root + ywin))
+
+    # Bind the title bar (B1 is the left mouse button <B1-Motion> and motion means that it will print the position of the moving mouse pointer)
+    title_bar.bind("<B1-Motion>", move_app)
+# Binds the left mouse button to the position of the window
+title_bar.bind("<Button-1>", get_pos)
+
+# Function for minimizing the app
+def minimize_app(e):
+    root.update_idletasks()
+    root.overrideredirect(False)
+    root.state('iconic')
+
+# Function for reminizing the app
+def reminimize_app(e):
+    root.update_idletasks()
+    root.overrideredirect(True)
+    root.state('normal')
+
+# Binds the titlebar so it reminimize the app
+title_bar.bind("<Map>", reminimize_app)
 
 # Custom title bar
-title_label = Label(title_bar, text="Note-App", background=figma_white, font=(inter_bold, 14), fg="black", bd=1)
-title_label.pack(side=LEFT, padx=50, pady=5)
+title_label = Label(title_bar, text="Note-App", background="black", font=(inter_bold, 14), fg=figma_white)
+title_label.pack(side=LEFT, padx=50)
 
-# Exit button for title bar
-close_label = Label(title_bar, text="X", bg=figma_white, fg="black", font=(inter_bold, 20))
-close_label.pack(side=RIGHT, padx=5)
+############################################# Feature Buttons ###########################################################
+
+# Text Button
+title_text = Button(title_bar, text="Text", background="black", font=(inter_regular, 10), fg=figma_white, relief="flat")
+title_text.pack(side=LEFT)
+
+# Audio Button
+title_audio = Button(title_bar, text="Audio", background="black", font=(inter_regular, 10), fg=figma_white, relief="flat")
+title_audio.pack(side=LEFT)
+
+# Image Button
+title_image = Button(title_bar, text="Image", background="black", font=(inter_regular, 10), fg=figma_white, relief="flat")
+title_image.pack(side=LEFT)
+
+# Link Button
+title_link = Button(title_bar, text="Link", background="black", font=(inter_regular, 10), fg=figma_white, relief="flat")
+title_link.pack(side=LEFT)
+
+############################################## Minimize, Maximize, Exit Buttons ########################################
+
+# Exit button
+close_label = Label(title_bar, image = exit_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
+close_label.pack(side=RIGHT, ipadx=5, ipady=5)
 close_label.bind("<Button-1>", quit_app)
 
+# Maximize Button
+close_label = Label(title_bar, image = maximize_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
+close_label.pack(side=RIGHT, ipadx=5, ipady=5)
+# close_label.bind("<Button-1>", quit_app)
+
+# Minimize Button
+close_label = Label(title_bar, image = minimize_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
+close_label.pack(side=RIGHT, ipadx=5, ipady=5)
+close_label.bind("<Button-1>", minimize_app)
+############################################# End of the application #######################################
 # App Loop IMPORTANT!!!
 root.mainloop()
-

b0027d30d191a761cdc0a94bdd4535eef39ea7fc
iDevelopedd <32646298+iDevelopedd@users.noreply.github.com>
2022-09-11 13:44:54 -0500

    Modifed Note-App.py to use Tkinter GUI

--- Note-App.py
+++ Note-App.py
@@ -1,140 +1,45 @@
-# Import the Tkinter Module
-from cgitb import text
 from tkinter import *
-import webbrowser
 
-############################################# Start of the application #####################
-# Set the root of the application
-root = Tk()
-
-root.title('Note-App')
-root.geometry("500x70")
-root.geometry("500x69")
-root.config(background="#D9D9D9")
-
-############################################# Setting up the variables and files we need #####################
 # Fonts and color scheme
 inter_bold = 'Inter-Bold'
 inter_regular = 'Inter-Regular'
 figma_white = '#D9D9D9'
 
-# Image importer for the minimize, maximize and exit buttons
-settings_button = PhotoImage(file = r"Images\Settings.png")
-exit_button = PhotoImage(file = r"Images\ExitButton.png")
-minimize_button = PhotoImage(file = r"Images\Minimize.png")
-maximize_button = PhotoImage(file= r"Images\Maximize.png")
+# Sets the root of the application
+root = Tk()
 
-# Resizes the images
-settings_button_image = settings_button.subsample(2, 2)
-exit_button_image = exit_button.subsample(2, 2)
-minimize_button_image = minimize_button.subsample(2, 2)
-maximize_button_image = maximize_button.subsample(2, 2)
-maximize_button_image = maximize_button.subsample(2, 2)
+# Configures the title, resoultion, icon image and background color
+root.title("Note-App")
+root.geometry("500x500")
+root.iconbitmap("Images\Icon.ico")
+root.config(background="#D9D9D9")
 
-###################################### Removing and creating a custom app ################################################
+# Sets the menubar to the root window
+menubar = Menu(root)
+root.config(menu=menubar)
 
-# Remove title bar
-root.overrideredirect(True)
+# Creates the file menu on the menubar
+fileMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="Open")
+fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Settings")
+fileMenu.add_command(label="Quit", command=root.quit)
 
-# Function for quitting the app
-def quit_app(e):
-    root.quit()
-    
-# Creates the title bar
-title_bar = Frame(root, bg=figma_white, relief="flat")
-title_bar.pack(fill=X, pady=5)
+# Creates the add text
+textMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Text", menu=textMenu)
+textMenu.add_command(label="Add Text")
 
-# Creates a frame for the title bar line
-title_line = Frame(root, bg="black", relief="flat")
-title_line.pack(expand=0, fill=X)
+# Creates the add image menu
+imageMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Image", menu=imageMenu)
+imageMenu.add_command(label="Add Image")
 
-# Function to get the current position of the mouse pointer on the screen
-def get_pos(e):
-    xwin = root.winfo_x()
-    ywin = root.winfo_y()
-    startx = e.x_root
-    starty = e.y_root
+# Creates the add link 
+linkMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Link", menu=linkMenu)
+linkMenu.add_command(label="Add Link")
 
-    ywin -= starty
-    xwin -= startx
-
-    # Function for the app window to the mouse pointer
-    def move_app(e):
-        root.geometry('+{0}+{1}'.format(e.x_root + xwin, e.y_root + ywin))
-
-    # Binds the title bar (B1 is the left mouse button <B1-Motion> and motion means that it will print the position of the moving mouse pointer)
-    title_bar.bind("<B1-Motion>", move_app)
-# Binds the left mouse button to the position of the window
-title_bar.bind("<Button-1>", get_pos)
-
-# Function for minimizing the app
-def minimize_app(e):
-    root.update_idletasks()
-    root.overrideredirect(False)
-    root.state('iconic')
-
-# Function for reminizing the app
-def reminimize_app(e):
-    root.update_idletasks()
-    root.overrideredirect(True)
-    root.state('normal')
-
-# Binds the titlebar so it reminimize the app
-title_bar.bind("<Map>", reminimize_app)
-
-def image_window():
-    image_root = Tk()
-
-    image_root.geometry("300x70")
-    image_root.title("Add Image")
-    image_root.resizable(0,0)
-    image_root.config(background=figma_white)
-
-    image_root.mainloop()
-
-# Custom title bar
-title_label = Label(title_bar, text="Note-App", background=figma_white, font=(inter_bold, 16), fg="black")
-title_label.grid(row=0, column=1, columnspan=3, sticky=SW)
-
-############################################# Feature Buttons ###########################################################
-
-# Settings Button
-title_setting = Button(title_bar, image = settings_button_image, background=figma_white, relief="flat")
-title_setting.grid(row=0, column=0, rowspan=3, padx=20)
-
-# Text Button
-title_text = Button(title_bar, text="Text", background=figma_white, font=(inter_regular, 10), fg="black", relief="flat")
-title_text.grid(row=1, column=1, sticky=NW)
-
-# Audio Button
-# title_audio = Button(title_bar, text="Audio", background=figma_white, font=(inter_regular, 10), fg="black", relief="flat", command=audio_window)
-# title_audio.grid(row=1, column=2)
-
-# Image Button
-title_image = Button(title_bar, text="Image", background=figma_white, font=(inter_regular, 10), fg="black", relief="flat", command=image_window)
-title_image.grid(row=1, column=3)
-
-# Link Button
-title_link = Button(title_bar, text="Link", background=figma_white, font=(inter_regular, 10), fg="black", relief="flat")
-title_link.grid(row=1, column=4)
-
-############################################## Minimize, Maximize, Exit Buttons ########################################
-
-# Exit button
-close_label = Label(title_bar, image = exit_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
-close_label.grid(row=0, column=7, padx=5, sticky=SE)
-close_label.bind("<Button-1>", quit_app)
-
-# Maximize Button
-max_label = Label(title_bar, image = maximize_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
-max_label.grid(row=0, column=6, padx=5, sticky=S)
-# max_label.bind("<Button-1>", maximize_app)
-
-# Minimize Button
-min_label = Label(title_bar, image = minimize_button_image, bg=figma_white, fg="black", font=(inter_bold, 20))
-min_label.grid(row=0, column=5, padx=5, sticky=S)
-min_label.bind("<Button-1>", minimize_app)
-
-############################################# End of the application #######################################
-# App Loop IMPORTANT!!!
-root.mainloop()
+# End of the application
+root.mainloop() # !!IMPORTANT

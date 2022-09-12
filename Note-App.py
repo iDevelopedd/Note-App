7b67fc702eff1c29a1245de70556ea566999393c
Kevin Alvarado <110209254+MrOctopus27@users.noreply.github.com>
2022-09-12 11:45:13 -0500

    Update Note-App.py

--- Note-App.py
+++ Note-App.py
@@ -1,17 +1,18 @@
 from tkinter import *
+import webbrowser
 
-<<<<<<< HEAD
-=======
+#<<<<<<< HEAD
+#=======
 ############################################# Start of the application #####################
 # Set the root of the application
-root = Tk()
+# root = Tk()
 
-root.title('Note-App')
-root.geometry("500x69")
-root.config(background="#D9D9D9")
+# root.title('Note-App')
+# root.geometry("500x69")
+# root.config(background="#D9D9D9")
 
 ############################################# Setting up the variables and files we need #####################
->>>>>>> d66094b9ae4c8ef8197af921d3b3a4a217284c43
+#>>>>>>> d66094b9ae4c8ef8197af921d3b3a4a217284c43
 # Fonts and color scheme
 inter_bold = 'Inter-Bold'
 inter_regular = 'Inter-Regular'
@@ -20,19 +21,13 @@ figma_white = '#D9D9D9'
 # Sets the root of the application
 root = Tk()
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 # Configures the title, resoultion, icon image and background color
 root.title("Note-App")
 root.geometry("500x500")
 root.iconbitmap("Images\Icon.ico")
 root.config(background="#D9D9D9")
-=======
-# Resizes the images
-settings_button_image = settings_button.subsample(2, 2)
-exit_button_image = exit_button.subsample(2, 2)
-minimize_button_image = minimize_button.subsample(2, 2)
-maximize_button_image = maximize_button.subsample(2, 2)
->>>>>>> d66094b9ae4c8ef8197af921d3b3a4a217284c43
+#>>>>>>> d66094b9ae4c8ef8197af921d3b3a4a217284c43
 
 # Sets the menubar to the root window
 menubar = Menu(root)
@@ -47,9 +42,24 @@ fileMenu.add_command(label="Settings")
 fileMenu.add_command(label="Quit", command=root.quit)
 
 # Creates the add text
+
+def textButton():
+
+    top = Toplevel()
+    top.resizable(False, False)
+    top.title("Text")
+    top.iconbitmap("Image\Icon.ico")
+
+    text = Text(top, height=15, width=90)
+    text.pack()
+
+    top.mainloop()
+
+
 textMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Text", menu=textMenu)
-textMenu.add_command(label="Add Text")
+textMenu.add_command(label="Add Text", command = textButton)
+
 
 # Creates the add image menu
 imageMenu = Menu(menubar, tearoff=0)
@@ -57,9 +67,20 @@ menubar.add_cascade(label="Image", menu=imageMenu)
 imageMenu.add_command(label="Add Image")
 
 # Creates the add link 
+def linkButton():
+    top = Toplevel()
+    top.resizable(False, False)
+    top.title("Link")
+    top.iconbitmap("Image\Icon.ico")
+
+    text = Text(top, height=15, width=90)
+    text.pack(pady=20)
+
+    top.mainloop()
+
 linkMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Link", menu=linkMenu)
-linkMenu.add_command(label="Add Link")
+linkMenu.add_command(label="Add Link", command = linkButton)
 
 # End of the application
 root.mainloop() # !!IMPORTANT
